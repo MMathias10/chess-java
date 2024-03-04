3b1272af240becb7173fd759c1fc27d2df906b39
Mateus Mathias <mateusmathiassilva10@gmail.com>
2024-03-04 18:42:49 -0300

    Colocando peças no tabuleiro e fazendo algumas exceptions

--- src/chess/ChessMath.java
+++ src/chess/ChessMath.java
@@ -1,20 +1,33 @@
 package chess;
 
 import boardgame.Board;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMath {
 	private Board board;
 	
 	public ChessMath() {
 		board = new Board(8, 8);
+		initialSetup();
 }
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
 		for(int i=0; i<board.getRows();i++) {
 			for (int j = 0;j<board.getColumns();j++) {
-				mat [i][j]=(ChessPiece) board.peice(i,j);
+				mat [i][j]=(ChessPiece) board.piece(i,j);
 			}
 		}
 		return mat;
+	}
+	private void placeNewpiece(char column,int row,ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	private void initialSetup() {
+		placeNewpiece('e', 8, new King(board,Color.BLACK));
+		placeNewpiece('e', 1, new King(board,Color.WHITE));
+		placeNewpiece('b', 6, new Rook(board,Color.BLACK));
+		
+
 	}
 }
