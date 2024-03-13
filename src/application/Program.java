3edfcb22821b8ff6d9c1653520e217ae98d05d4a
Mateus Mathias <mateusmathiassilva10@gmail.com>
2024-03-12 21:11:40 -0300

    tratamento de exceção e ClearScreen

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMath;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,16 +15,24 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMath chessmath = new ChessMath();
 		while(true) {
-			UI.printBoard(chessmath.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.println("target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessmath.performChessMove(source, target);
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessmath.getPieces());
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.println("target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessmath.performChessMove(source, target);
+			}
+			catch (ChessException e) {
+				// TODO: handle exception
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 		
 
